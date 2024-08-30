cf306b51444062ea84d05a724026b6fc0d82917d
mani <jvenkatamani9999@gmail.com>
2024-08-31 05:29:14 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173, 
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: parseInt(env.VITE_PORT) || 5173,
+    },
+  }
 })
